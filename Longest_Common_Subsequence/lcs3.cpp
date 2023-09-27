789ff0fb164b5eb258ee57b564265ddcbab9d520
Xabier Fernandez <xabier.fernandez@outlook.com>
2023-09-28 01:32:07 +0200

    Update algorithms

--- Longest_Common_Subsequence/lcs3.cpp
+++ Longest_Common_Subsequence/lcs3.cpp
@@ -1,31 +1,57 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
+using namespace std;
 using std::vector;
 
-int lcs3(vector<int> &a, vector<int> &b, vector<int> &c) {
+int lcs_3(vector<int> &a, vector<int> &b, vector<int> &c) {
   //write your code here
-  return std::min(std::min(a.size(), b.size()), c.size());
+  int n = a.size();
+  int m = b.size();
+  int o = c.size();
+  int dp[n +1][m +1][o +1];
+  int i, j, k;
+
+  for (i = 0; i <= n; i++) 
+  { 
+      for (j = 0; j <= m; j++) 
+      { 
+        for (k = 0; k <= o; k++){
+          if(i == 0 || j == 0 || k == 0) 
+              dp[i][j][k] = 0; 
+          else if (a[i-1] == b[j-1] && a[i-1] == c[k-1]) 
+              dp[i][j][k] = dp[i-1][j-1][k-1] + 1; 
+          else
+              dp[i][j][k] = max(max(dp[i-1][j][k], 
+                                dp[i][j-1][k]),
+                                dp[i][j][k-1]); 
+        }
+          
+      } 
+  } 
+
+  return dp[n][m][o];
 }
 
 int main() {
   size_t an;
-  std::cin >> an;
+  cin >> an;
   vector<int> a(an);
   for (size_t i = 0; i < an; i++) {
-    std::cin >> a[i];
+    cin >> a[i];
   }
   size_t bn;
-  std::cin >> bn;
+  cin >> bn;
   vector<int> b(bn);
   for (size_t i = 0; i < bn; i++) {
-    std::cin >> b[i];
+    cin >> b[i];
   }
   size_t cn;
-  std::cin >> cn;
+  cin >> cn;
   vector<int> c(cn);
   for (size_t i = 0; i < cn; i++) {
-    std::cin >> c[i];
+   cin >> c[i];
   }
-  std::cout << lcs3(a, b, c) << std::endl;
+  cout << lcs_3(a, b, c) << endl;
 }

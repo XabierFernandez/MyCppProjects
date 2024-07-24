6d5ad50cbebb078f6c64711d7951ae1d56d569bd
Xabier Fernandez <xabier.fernandez@outlook.com>
2024-07-24 17:57:12 +0200

    Update

--- Employee/employee.cpp
+++ Employee/employee.cpp
@@ -22,6 +22,6 @@ public:
 };
 
 int main() {
-  Employee employe01 = Employee("Xabier", "Fernandez", 43);
+  Employee employe01 = Employee("Xabier", "Fernandez", 44);
   employe01.printEmployeeDetails();
 }

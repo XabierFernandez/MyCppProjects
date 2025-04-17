82e40f1379fe0ffb111cf692bd06b49a867ab573
Xabier Fernandez <xabier.fernandez@outlook.com>
2025-04-17 20:32:42 +0200

    New exaamples added for Networking & Kernel drivers

---
+++ Linux_Device_Drivers/.Module.symvers.cmd
@@ -0,0 +1,1 @@
+savedcmd_Module.symvers :=  /usr/src/kernels/6.13.10-200.fc41.x86_64/scripts/mod/modpost -M        -o Module.symvers -n -T modules.order -i /usr/src/kernels/6.13.10-200.fc41.x86_64/Module.symvers -e 
---
+++ Linux_Device_Drivers/.hello_world.ko.cmd
@@ -0,0 +1,1 @@
+savedcmd_hello_world.ko := ld -r -m elf_x86_64 -z noexecstack --no-warn-rwx-segments --build-id=sha1  -T /usr/src/kernels/6.13.10-200.fc41.x86_64/scripts/module.lds -o hello_world.ko hello_world.o hello_world.mod.o .module-common.o
